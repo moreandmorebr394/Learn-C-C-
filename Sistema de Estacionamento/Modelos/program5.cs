84c1442a4bf6b18c1a2043483999eaedd4b8f3ca
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2024-07-26 12:45:23 -0300

    Add files via upload

---
+++ Sistema de Estacionamento/Modelos/program5.cs
@@ -0,0 +1,13 @@
+using ExemploFundamentos.Models
+
+bool choveu = false;
+bool estaTarde = false;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia");
+}
