ab1d243b48850c0a3369440de81bc8b64b1d083e
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2024-07-28 11:46:55 -0300

    Update Estacionamento.cs

--- Estacionamento.cs
+++ Estacionamento.cs
@@ -17,5 +17,32 @@ namespace Sistema_de_Estacionamento.Models {
 
         public void RemoverVeiculo() {
             Console.WriteLine("Digite a placa do veículo para remover:");
-            string placa = Console.ReadLine
+              string placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Any(x = > x.ToUpper() == placa.ToUpper())) {
+                ConsoleWrite($"Digite a quantiddade de horas que o veículo permaneceu estacionado:");
+
+                int horas = int.Parse(Console.ReadLIne());
+                decimal valorTotal = percoInicial + precoPorHoras * horas'
+
+                veiculos.Remover(placa);
+                Console.WriteLine($"Oveículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+             else
+                 Console.WriteLine("Veículo não encontrado. Confira se digitou a placa corretamente")'
+                 
+        }
+    public void ListarVeiculos() {
+        if (veiculos.any()) {
+            Console.WriteLine("Os veículos estacionados são: ");
+
+            foreach (string placa in veiculos) {
+                Console.WriteLine(placa);
+            }
+    }
+        else
+            Console.WriteLine("Não há veículos estacionados.")
+        }
+    }
+
 }
